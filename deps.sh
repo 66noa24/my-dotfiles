37ae6c32cb43fca254031ff077bc7e4ffd2290a3
66noa24 <noahmk4605@protonmail.com>
2025-09-05 22:46:52 -0400

    up

--- deps.sh
+++ deps.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 # 090525 - en6624 script for i3 and general dependencies
 
-echo "Options: i3-1 (j i3), main-2, x11vnc-3, update system-4"
-read -r -p "Choice 1-4: " ch
-case $ch in
-  1) 13_inst ;;
-  2) main_inst ;;
-  3) vnc_inst ;;
-  4) sudo xbps-isntall -Syu ;;
-  *) echo "not valid option" ;;
-esac
-
 i3_inst() {
   sudo xbps-install i3
 }
@@ -22,3 +12,14 @@ main_inst() {
 vnc_inst() {
   sudo xbps-install x11vnc
 }
+
+# Other context
+echo "Options: i3-1 (j i3), main-2, x11vnc-3, update system-4"
+read -r -p "Choice 1-4: " ch
+case $ch in
+  1) 13_inst ;;
+  2) main_inst ;;
+  3) vnc_inst ;;
+  4) sudo xbps-isntall -Syu ;;
+  *) echo "not valid option" ;;
+esac
