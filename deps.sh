b138a68c88997e5416fc7fff8f63c26ddc168ae7
66noa24 <noahmk4605@protonmail.com>
2025-09-05 22:32:47 -0400

    update setup.sh

---
+++ deps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 090525 - en6624 script for i3 and general dependencies
+
+echo "Options: i3-1 (j i3), main-2, x11vnc-3, update system-4"
+case $ch in
+  1) 13_inst ;;
+  2) main_inst ;;
+  3) vnc_inst ;;
+  4) sudo xbps-isntall -Syu
+  *) echo "not valid option" ;;
+esac
+
+i3_inst() {
+  sudo xbps-install i3
+}
+
+main_inst() {
+  sudo xbps-install i3 conky polybar emptty helix kitty nitrogen dmenu x11vnc
+}
+
+vnc_inst() {
+  sudo xbps-install x11vnc
+}
